a36a4ecc4b640c3ce3ad6afd41a186b15351e034
Jacob Madden <jacob.madden@connectsolutions.com>
2014-10-22 14:26:36 -0700

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+var paths = {
+  tests: 'test/**/*_test.js'
+};
+
+gulp.task('test', function () {
+  process.env.NODE_ENV = 'test';
+
+  return gulp.src(paths.tests, { read: false }).pipe(mocha({ reporter: 'spec', bail: true }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch([paths.scripts, paths.tests], ['test']);
+});
+
+gulp.task('tests', ['watch', 'test']);
