aa14e345f2d6d672f1112bcdb559f0920a90d3d9
Jodessa Lanzaderas <jodessa.lanzaderas@connectsolutions.com>
2014-10-22 20:25:30 -0700

    using keyword-extractor for context keyword retrieval

--- app/router.js
+++ app/router.js
@@ -1,9 +1,9 @@
-var express  = require('express');
-var glossary = require ('glossary');
-var extractor  = require ('keyword-extract');
-var server   = require('./server');
-var logger   = require('./common/logger');
-var say      = require('say');
+var express   = require('express');
+var glossary  = require('glossary');
+var extractor = require('keyword-extractor');
+var server    = require('./server');
+var logger    = require('./common/logger');
+var say       = require('say');
 
 module.exports = function() {
 
@@ -22,12 +22,15 @@ module.exports = function() {
 
       var result = [];
       var method = req.body.method;
-      if (method === 'glossary') {
+      if (method === 'glossary') {  //context 1
         result = glossary.extract(req.body.phrase);
+      } else if (method === 'keyword-extractor') {   //context 2
+        result = extractor.extract(req.body.phrase, {language:"english", return_changed_case:true});
       } else if (method === 'frequency') {
         logger.debug('IN PROGRESS');
       } else {
-        result = glossary.extract(req.body.phrase);
+        //result = glossary.extract(req.body.phrase);
+        result = extractor.extract(req.body.phrase, {language:"english", return_changed_case:true});
       }
 
       var output = req.body.output;
