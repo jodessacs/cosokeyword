217e8a641ca4929cb14f899b0041a3887928096c
Jacob Madden <jacob.madden@connectsolutions.com>
2014-10-23 13:42:28 -0700

    hapi and backbone stuff

---
+++ hapi/public/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Cosokeyword, $*/
+
+
+window.Cosokeyword = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Cosokeyword.init();
+});
