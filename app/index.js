3b30773b82e9963a728434a4e95afa84eab76524
Jodessa Lanzaderas <jodessa.lanzaderas@connectsolutions.com>
2014-10-23 10:46:12 -0700

    make port configurable

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ var server = require('./server');
 var config = require('config');
 var logger = require('./common/logger');
 
-server.listen(process.env.PORT ? process.env.PORT : config.get('port'), function () {
-  logger.debug('Example app listening on port %s', config.get('port'));
+var theServer = server.listen(process.env.PORT ? process.env.PORT : config.get('port'), function () {
+  logger.debug('Example app listening on port %s', theServer.address().port);
 });
