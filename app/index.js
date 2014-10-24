f9b5334e5cf2278328ac523a0ed15bd660fe5139
Jacob Madden <jacob.madden@connectsolutions.com>
2014-10-23 17:06:35 -0700

    port log msg

--- app/index.js
+++ app/index.js
@@ -3,5 +3,5 @@ var config = require('config');
 var logger = require('./common/logger');
 
 server.start(function () {
-  logger.debug('Starting server');
+  logger.debug('Starting server:' + server.info.uri + ' with port:' + process.env.PORT);
 });
