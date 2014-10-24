6f314347f59278e22f884988dd724adf1b232417
Jacob Madden <jacob.madden@connectsolutions.com>
2014-10-24 13:26:24 -0700

    workaround, add dist to git

---
+++ public/dist/scripts/37f053b1.main.js
@@ -0,0 +1,1 @@
+window.Cosokeyword={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";console.log("Hello from Backbone!")}},$(document).ready(function(){"use strict";Cosokeyword.init()}),this.JST=this.JST||{};
